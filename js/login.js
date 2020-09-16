8ae6dc136a464dda766493c501d1fc1862b92b47
Ryan <mcmasteroreo77@gmail.com>
2020-09-16 18:11:39 -0400

    Removes unnecessary function

--- js/login.js
+++ js/login.js
@@ -1,14 +1,10 @@
-const doLogin = () => {
-    login();
-}
-
-const login = async () => {
+const doLogin = async () => {
     const username = document.getElementById('login-name').value;
     const password = document.getElementById('login-password').value;
     const { data, error } = await loginAccount(username, password);
     if (data) {
         setActiveUser(data);
-        window.location.href = '/ContactSearch.html';
+        window.location.href = '/home.html';
     } else {
         document.getElementById('login-error').innerHTML = 'Invalid username or password';
     }
