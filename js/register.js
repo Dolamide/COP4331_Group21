d01d49e9818df5e4e6132ae6805d1dd83069e73e
adamloree <58012816+adamloree@users.noreply.github.com>
2020-09-21 14:50:45 -0400

    JS for register page
    It looks pretty gross, if anyone wants it to look better I could pimp my code.

---
+++ js/register.js
@@ -0,0 +1,16 @@
+const doRegister = async () => {
+    const username = document.getElementById('user-name').value;
+    const password = document.getElementById('login-password').value;
+    const first_name = document.getElementById('name').value;
+    const last_name = document.getElementById('lastname').value;
+    if(!(username === "" || password === "" || first_name === "" || last_name === "")) {
+    const { data, error } = await addAccount(username, password, first_name, last_name);
+      if (!(error === "")) {
+          document.getElementById('login-result').innerHTML = '<p>Account exists</p>';
+      } else {
+          document.getElementById('login-result').innerHTML = '<p>Account created successfully</p>';
+      }
+    } else {
+      document.getElementById('login-result').innerHTML = "<p>Incomplete entry</p>"
+    }
+}
