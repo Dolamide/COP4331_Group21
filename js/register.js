8ba9c9bd64495dc17bfc67ab2eb2d17792c6c663
adamloree <adamloree@gmail.com>
2020-09-27 18:26:29 -0400

    Register page error update

--- js/register.js
+++ js/register.js
@@ -1,3 +1,12 @@
+// Get the modal
+var modal = document.getElementById("myModal_Reg");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close_Reg")[0];
+
 const doRegister = async () => {
     const username = document.getElementById('user-name').value;
     const password = document.getElementById('login-password').value;
@@ -7,12 +16,26 @@ const doRegister = async () => {
     const { data, error } = await addAccount(username, password, first_name, last_name);
       if (!(error === "")) {
           document.getElementById('modalError').innerHTML = 'Account exists';
-          $("#myModal").modal('show');
+          modal.style.display = "block";
       } else {
           window.location.href = "/loginpage.html";
       }
     } else {
       document.getElementById('modalError').innerHTML = "Incomplete entry";
-      $("#myModal").modal('show');
+      modal.style.display = "block";
     }
+}
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
