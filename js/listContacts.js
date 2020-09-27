b2db7f0efcd253768df9b143e867e9d1da10f2ac
adamloree <58012816+adamloree@users.noreply.github.com>
2020-09-27 16:46:26 -0400

    Changed to function

--- js/listContacts.js
+++ js/listContacts.js
@@ -1,46 +1,50 @@
 document
   .getElementById("searchbutton")
   .addEventListener("click", async (event) => {
-    event.preventDefault();
-
-    const user_id = getActiveUser().user_id;
-
-    const query = document.getElementById("search").value;
-
-    console.log("query: ", query);
-
-    const apiResult = await searchContact(user_id, query);
-    const contacts = apiResult.data;
-    const error = apiResult.error;
-
-    console.log(apiResult);
-
-    //checking for contact list that already exists
-    document.getElementById("show-panel").innerHTML = "";
-    console.log(contacts);
-    if (!error) {
-      document.getElementById("show-panel").innerHTML +=
-        '<tr><th class="tl column tableheader tlfname">First Name</th><th class="tl column tableheader tllname">Last Name</th><th class="tl column tableheader phone">Phone Number</th><th class="tl column tableheader email">Email</th></tr>';
-      for (var i = 0; i < contacts.length; i++) {
-        document.getElementById("show-panel").innerHTML +=
-          "<td>" +
-          contacts[i].first_name +
-          "</td>" +
-          "<td>" +
-          contacts[i].last_name +
-          "</td>" +
-          "<td>" +
-          contacts[i].phone_number +
-          "</td>" +
-          "<td>" +
-          contacts[i].email +
-          "</td>" +
-          '<td><a href="editcontact.html?contact_id=' +
-          contacts[i].contact_id +
-          '" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a><a class="delete" title="Delete" data-toggle="tooltip" id="myBtn" ><i class="material-icons">&#xE872;</i></a></td>';
-      }
-    } else {
-      document.getElementById("show-panel").innerHTML +=
-        '<div class="contact-item">You have no contacts. Would you like to add a new contact?</div><hr>';
-    }
+    listContactsFunct();
   });
+
+async function listContactsFunct() {
+  event.preventDefault();
+
+  const user_id = getActiveUser().user_id;
+
+  const query = document.getElementById("search").value;
+
+  console.log("query: ", query);
+
+  const apiResult = await searchContact(user_id, query);
+  const contacts = apiResult.data;
+  const error = apiResult.error;
+
+  console.log(apiResult);
+
+  //checking for contact list that already exists
+  document.getElementById("show-panel").innerHTML = "";
+  console.log(contacts);
+  if (!error) {
+    document.getElementById("show-panel").innerHTML +=
+      '<tr><th class="tl column tableheader tlfname">First Name</th><th class="tl column tableheader tllname">Last Name</th><th class="tl column tableheader phone">Phone Number</th><th class="tl column tableheader email">Email</th></tr>';
+    for (var i = 0; i < contacts.length; i++) {
+      document.getElementById("show-panel").innerHTML +=
+        "<td>" +
+        contacts[i].first_name +
+        "</td>" +
+        "<td>" +
+        contacts[i].last_name +
+        "</td>" +
+        "<td>" +
+        contacts[i].phone_number +
+        "</td>" +
+        "<td>" +
+        contacts[i].email +
+        "</td>" +
+        '<td><a href="editcontact.html?contact_id=' +
+        contacts[i].contact_id +
+        '" class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a><a class="delete" title="Delete" data-toggle="tooltip" id="myBtn" ><i class="material-icons">&#xE872;</i></a></td>';
+    }
+  } else {
+    document.getElementById("show-panel").innerHTML +=
+      '<div class="contact-item">You have no contacts. Would you like to add a new contact?</div><hr>';
+  }
+}
