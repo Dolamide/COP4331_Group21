254be7a2f039a2f4a1e7e5431cc093f02b39b88e
adamloree <adamloree@gmail.com>
2020-09-27 22:02:17 -0400

    Erased modal

--- js/register.js
+++ js/register.js
@@ -1,9 +1,3 @@
-// Get the modal
-var modal = document.getElementById("myModal_Reg");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close_Reg")[0];
-
 const doRegister = async () => {
     const username = document.getElementById('user-name').value;
     const password = document.getElementById('login-password').value;
@@ -12,27 +6,11 @@ const doRegister = async () => {
     if(!(username === "" || password === "" || first_name === "" || last_name === "")) {
     const { data, error } = await addAccount(username, password, first_name, last_name);
       if (!(error === "")) {
-          document.getElementById('modalError').innerHTML = 'Account exists';
-          modal.style.display = "block";
+          document.getElementById('login-error').innerHTML = 'Account exists';
       } else {
           window.location.href = "/loginpage.html";
       }
     } else {
-      document.getElementById('modalError').innerHTML = "Incomplete entry";
-      modal.style.display = "block";
+      document.getElementById('login-error').innerHTML = "Incomplete entry";
     }
-}
-
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
